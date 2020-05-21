be89bed748c9b4c2b040dd47b7f42ef7e6713e4e
Charles Chen <martian2005@gmail.com>
2020-05-22 07:10:19 +1000

    [update] add frontend code

--- backend-project/src/decorator/use.ts
+++ backend-project/src/decorator/use.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
 import { RequestHandler } from "express";
-import { CrowllerController, LoginController } from "../controller";\
+import { CrowllerController, LoginController } from "../controller";
 
 export function use(middlewear: RequestHandler) {
   return function (target: CrowllerController | LoginController, key: string) {
-    const originMiddlewares = Reflect.getMetadata("middlewears", target, key) || [];
+    const originMiddlewares =
+      Reflect.getMetadata("middlewears", target, key) || [];
     originMiddlewares.push(middlewear);
     Reflect.defineMetadata("middlewears", originMiddlewares, target, key);
   };
