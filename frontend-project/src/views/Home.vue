7f602726882a729779dae31bc6071bfacb6ac162
Charles Chen <martian2005@gmail.com>
2020-05-22 09:51:10 +1000

    [update] update frontend code

---
+++ frontend-project/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-page">
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from "@/components/Login.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Login,
+  },
+};
+</script>
+<style scoped>
+.login-page {
+  width: 300px;
+  padding: 20px;
+  margin: 100px auto;
+  border: 1px solid #ccc;
+}
+</style>
