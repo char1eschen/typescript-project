cc5c783340a24e7006269499779511a4f5197141
Charles Chen <martian2005@gmail.com>
2020-05-25 22:44:27 +1000

    [update] add react app

---
+++ frontend-project-react/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// function App() {
+//   return <div>hello world</div>;
+// }
+const App: React.FC = () => {
+  return <div>HELLO WORLD</div>;
+};
+
+export default App;
